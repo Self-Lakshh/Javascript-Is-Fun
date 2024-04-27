0ca1dd7e7ebec4f8a1bea83fd4ae21f522e15807
Self-Lakshh <lakshyachopra2004@gmail.com>
2024-04-27 04:53:00 +0000

    practice array functional methods map filter reduce exercises

--- Day-021/script.js
+++ Day-021/script.js
@@ -1,1 +1,11 @@
-// Learning: Array functional methods: map, filter, reduce
+// Day 21: Array functions: map, filter, reduce (Reference: W3Schools Array Methods)
+const numbers = [1, 2, 3, 4, 5];
+
+const doubled = numbers.map(num => num * 2);
+console.log("Doubled array:", doubled);
+
+const evens = numbers.filter(num => num % 2 === 0);
+console.log("Evens list:", evens);
+
+const sum = numbers.reduce((acc, curr) => acc + curr, 0);
+console.log("Sum aggregated:", sum);
