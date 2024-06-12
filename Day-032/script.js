7ff1b6d543fa36552e3e42aa9b4c9d7dda5516b2
Self-Lakshh <lakshyachopra2004@gmail.com>
2024-06-12 04:47:00 +0000

    practice control flow conditional statements practice exercises

--- Day-032/script.js
+++ Day-032/script.js
@@ -1,1 +1,10 @@
-// Learning: Control Flow: conditional statements practice
+// Day 32: Control Flow Practice - Check positive/negative/zero
+let number = -5;
+
+if (number > 0) {
+    console.log("Number is positive");
+} else if (number < 0) {
+    console.log("Number is negative");
+} else {
+    console.log("Number is zero");
+}
