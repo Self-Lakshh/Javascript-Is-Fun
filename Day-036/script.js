624e88883172fbf87fb9c91e45c4647394872c2d
Self-Lakshh <lakshyachopra2004@gmail.com>
2024-06-30 04:45:00 +0000

    practice objects practices and key enumeration exercises

--- Day-036/script.js
+++ Day-036/script.js
@@ -1,1 +1,10 @@
-// Learning: Objects practices and key enumeration
+// Day 36: Learning Objects - Keys, values, and methods
+const car = {
+  make: "Toyota",
+  model: "Corolla",
+  year: 2022
+};
+
+console.log("Car Keys:", Object.keys(car));
+console.log("Car Values:", Object.values(car));
+console.log("Has model property?", car.hasOwnProperty("model"));
