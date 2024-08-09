65cb9c447e8ff7fff797048e98b37e62ca2eeb0b
Self-Lakshh <lakshyachopra2004@gmail.com>
2024-08-09 06:06:00 +0000

    practice es6 modules practices and structures exercises

--- Day-046/script.js
+++ Day-046/script.js
@@ -1,1 +1,26 @@
-// Learning: ES6 Modules practices and structures
+// Day 46: ES6 Modules Practice
+console.log("Day 46: ES6 Modules Practice");
+
+const module1 = (() => {
+    const PI = 3.14159;
+    
+    function calculateCircleArea(radius) {
+        return PI * radius * radius;
+    }
+    
+    return { PI, calculateCircleArea };
+})();
+
+class User {
+    constructor(name, email) {
+        this.name = name;
+        this.email = email;
+    }
+    
+    displayInfo() {
+        console.log("User: " + this.name + ", Email: " + this.email);
+    }
+}
+
+console.log('PI:', module1.PI);
+console.log('Circle area (radius 5):', module1.calculateCircleArea(5));
